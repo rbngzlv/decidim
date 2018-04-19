8e3b1347d310bab6537a128d878dfafd028d3a7f
Ruben Gonzalez <rbngzlv@gmail.com>
2018-04-19 16:53:00 +0200

    Apply final design for public view

--- decidim-assemblies/app/presenters/decidim/assembly_member_presenter.rb
+++ decidim-assemblies/app/presenters/decidim/assembly_member_presenter.rb
@@ -5,24 +5,46 @@ module Decidim
   # Decorator for assembly members
   #
   class AssemblyMemberPresenter < SimpleDelegator
+    def age
+      (Time.current.strftime("%Y%m%d").to_i - birthday.strftime("%Y%m%d").to_i) / 10_000 if birthday
+    end
+
+    delegate :profile_url, :avatar_url, to: :user, allow_nil: true
+
     def gender
-      I18n.t(__getobj__.gender, scope: "decidim.admin.models.assembly_member.genders", default: "-")
+      I18n.t(__getobj__.gender, scope: "decidim.admin.models.assembly_member.genders", default: nil)
     end
 
     def name
-      name = __getobj__.full_name
+      user ? user.name : full_name
+    end
 
-      if (user = __getobj__.user.presence)
-        name << " (#{Decidim::UserPresenter.new(user).nickname})"
-      end
+    def nickname
+      user.nickname if user
+    end
 
-      name
+    def personal_information
+      [
+        gender,
+        age,
+        origin.presence
+      ].compact.join(" / ")
     end
 
     def position
-      return __getobj__.position_other if __getobj__.position == "other"
+      return position_other if __getobj__.position == "other"
 
-      I18n.t(__getobj__.position, scope: "decidim.admin.models.assembly_member.positions", default: "-")
+      I18n.t(__getobj__.position, scope: "decidim.admin.models.assembly_member.positions", default: "")
+    end
+
+    private
+
+    def user
+      @user ||= begin
+        if (user = __getobj__.user.presence)
+          Decidim::UserPresenter.new(user)
+        end
+      end
     end
   end
 end
